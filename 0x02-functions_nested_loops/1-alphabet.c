47f4ad14a9201c3ebf4ac20a4e193f086fb19cf6
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-07 14:03:57 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main-prints alphabets in lowercase'
+ * followed by a new line
+ * Return:0
+ */
+void print_alphabet(void)
+
+{
+	char alphabets;
+
+	alphabets = 'a';
+	while (alphabets <= 'z');
+	{
+		_putchar(alphabets);
+		alphabets++;
+	}
+	_putchar('\n');
+}
