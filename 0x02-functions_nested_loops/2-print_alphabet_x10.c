d6c43135ea3a086061facfd0d6bdfd3340a3c684
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-07 15:00:19 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**print_alphabet_x10 - prints alphabet ten times in lowercase'
+ * followed by a new line
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (c = 'a'; c ='z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
