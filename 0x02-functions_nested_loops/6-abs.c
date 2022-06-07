cfba5bf4e1ffe3ec982d6e375226627351d21d09
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-07 16:32:42 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/** _abs - prints absolute value of an integer
+ * Return: Integer
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
