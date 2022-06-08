ec069b4be97e1baed188b778b4733a5ace34ad05
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-08 03:21:46 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - gets sum of two integers and return the result
+ * @:a integer a
+ * @b: integer b
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
