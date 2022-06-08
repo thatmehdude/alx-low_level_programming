fad58f167e586417a95ad00dbbe77f4ce9777c51
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-08 01:08:53 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @r: integer input
+ * Return: value of last digit
+ */
+int print_last_digit(int r)
+{
+	int last_digit;
+	if (r > 0)
+		last_digit = r % 10;
+	else 
+		last_digit = -1 * (r % 10);
+	_putchar((last_digit % 10) + '0');
+	return (last_digit % 10);
+}
