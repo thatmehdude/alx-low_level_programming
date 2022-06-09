c9cdf27684b9d3e2894b835abab6f698a02086e8
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-09 10:28:18 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most-numbers - prints numbers from 0 - 9
+ * except 2 and 4
+ * followed by a new line
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		if (i != 2 && i != 4)
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
