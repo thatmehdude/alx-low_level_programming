1df24f7b6266a8872c4707a0d9776cc31e420ea2
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-09 15:53:43 -0700

    task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -20,7 +20,7 @@ int main(void)
 		max = 2;
 		n /= 2;
 	}
-	for (i = 3; i <= sqrt(n); i= i + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
@@ -31,7 +31,8 @@ int main(void)
 	
 	if (n > 2)
 		max = n;
-	printf("ld\n", max);
+	
+	printf("%ld\n", max);
 
 	return (0);
 }
