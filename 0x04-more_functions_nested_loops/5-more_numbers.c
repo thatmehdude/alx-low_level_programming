1df367ab3ac4c92f9c1919843dc01478073638fc
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-09 12:23:14 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 	char = num;
 	int = lines;
 
-	for (lines = '0'; lines <= '9'; lines ++)
+	for (lines = '0'; lines <= '9'; lines++)
 	{
-		for (num = 0; num <= 14; num ++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num / 10 > 0)
 			{
