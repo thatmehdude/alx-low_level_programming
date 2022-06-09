7df20efedc27c564d15e72427b3317c727b4c98c
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-09 14:26:16 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -27,6 +27,9 @@ int main(void)
 			printf(" FizzBuzz");
 		}
 		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
 		{
 			printf(" %d", i);
@@ -36,5 +39,3 @@ int main(void)
 
 	return (0);
 }
-
-
