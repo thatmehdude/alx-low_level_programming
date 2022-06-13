d259929fa5fcb63c49a328957d5e623d5156da12
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-13 13:40:54 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	char str[50];
+
+	printf("%s", strrev(str));
+}
