2c980a4b6e1c639b02aed2b646dccb845d9d0f14
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-13 12:26:38 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - gets the length of a string
+ * @s: parameter
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (0);
+}
