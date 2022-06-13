015d4dde64059d3bc328495c1dddfdc6495ab835
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-13 13:46:37 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,16 @@
  */
 void print_rev(char *s)
 {
-	char str[50];
+	int length = 0;
 
-	printf("%s", strrev(str));
+	while (length >= 0)
+	{
+		if (s[length] == '\0')
+			break;
+		length++;
+	}
+
+	for (length--; length>= 0; length--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
