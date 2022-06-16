34f02f01f899b04ee569ed19304d6dc9f8d2b959
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-16 07:24:03 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,19 +6,19 @@
  *
  * Return: resulting string
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i, j;
-	
+
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
-		 ')', '{', '}'};
+		')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
-		for (j = 0; j< 13; j++)
+		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == spe[j])
 			{
