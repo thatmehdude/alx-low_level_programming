155f560b19396600f6f06d6ce4fe818ff574fe20
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-23 11:46:22 -0700

    task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int cmp(char *c1, char *c2);
+
+/**
+ * escwild - escapes wild card
+ * @ct: string 1
+ * @wild: string 2
+ * Return: go through string 1 until match or '\0' is found
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*c1 == '\0')
+		return (cmp(c1, wid));
+	return (cmp(c1, wild) || escwild(++c1, wild));
+}
+
+/**
+ * cmp - cpmpares strings
+ * @c1: string 1
+ * @c2: string 2
+ * Return: 1 if match and 0 if o match
+ */
+int cmp(char *c1, char *c2)
+{
+	if (*c1 == *c2 || *c2 == '*')
+	{
+		if (*c2 == '*')
+			return (escwild(c1, ++c2));
+		else if (*c1 == '\0')
+			return (1);
+		else
+			return (cmp(++c1, ++c2));
+	}
+	return (0);
+}
+
+/**
+ * wildcmp - compares  strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: return 0 if no match and 1 if match
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (cmp(s1, s2));
+}
