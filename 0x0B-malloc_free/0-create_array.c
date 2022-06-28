f3112c5e4d4321381061a514adf0aee4a869ce3e
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-28 07:23:48 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates char arrays
+ * @size: size of array
+ * @c: char to be stored
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cp;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cp = malloc (sizeof(c) * size);
+
+	if (cp == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		cp[i] = c;
+	}
+	return (cp);
+}
