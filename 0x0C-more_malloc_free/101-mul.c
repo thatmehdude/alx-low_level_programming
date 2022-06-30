685a2cf6454dd29bb6591bf31c528d18d22ed016
Chisom Anaele <anaelechisom16@gmail.com>
2022-06-30 16:20:35 -0700

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,7 +25,7 @@ int is_digit(char *s)
  * _strlen - returns the length of a string
  * @s: string to evaluate
  *
- * Returnn: the length of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -39,7 +39,7 @@ int _strlen(char *s)
 }
 
 /**
- * errors - haddles errors for main
+ * errors - handles errors for main
  */
 void errors(void)
 {
