3d32245a3826549e04b4a656f83b474d8a48a77a
Chisom Anaele <anaelechisom16@gmail.com>
2022-07-04 11:12:25 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - points to program name
+ * @argc: count of arguments in program
+ * @argv: array of pointer to strings
+ * Return: zero
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("Program's name is %s", argv[0]);
