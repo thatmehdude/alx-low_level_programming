7d9b18737fd5dde0fd2e181a88af745623d473df
Chisom Anaele <anaelechisom16@gmail.com>
2022-07-25 20:15:38 +0100

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile - reads a text file
  * @filename: path of the file to read
  * @letters: number of letters to read and print
- * Return: number of letters readed.
+ * Return: number of letters read.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
